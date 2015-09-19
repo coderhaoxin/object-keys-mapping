8ce8b422d15d891bdef41e34221806f1c3dec2b6
haoxin <coderhaoxin@outlook.com>
2015-09-19 16:53:24 +0800

    v1.0.0 :dancer:
      - If origin not valid object, return {}. closes #3
      - test++, add coverage, add badges. closes #2

--- Readme.md
+++ Readme.md
@@ -1,4 +1,8 @@
 
+[![NPM version][npm-img]][npm-url]
+[![License][license-img]][license-url]
+[![Dependency status][david-img]][david-url]
+
 ### object-keys-mapping
 
 ### Usage
@@ -42,3 +46,10 @@ const obj = operator.map({
 
 ### License
 MIT
+
+[npm-img]: https://img.shields.io/npm/v/object-keys-mapping.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/object-keys-mapping
+[license-img]: https://img.shields.io/badge/license-MIT-green.svg?style=flat-square
+[license-url]: http://opensource.org/licenses/MIT
+[david-img]: https://img.shields.io/david/onebook/object-keys-mapping.svg?style=flat-square
+[david-url]: https://david-dm.org/onebook/object-keys-mapping
--- test/index.js
+++ test/index.js
@@ -38,6 +38,7 @@ describe('object-keys-mapping', () => {
 
   describe('mapping', () => {
     const origin = {
+      a: 'hi',
       a_b: 'a',
       a_c: {
         b_c: 'b',
@@ -46,10 +47,12 @@ describe('object-keys-mapping', () => {
         }
       },
       a_d: [1, 2, 3],
-      a_e: [{ title: 'hi' }, { title: 'hi' }, { title: 'hi' }]
+      a_e: [{ title: 'hi' }, { title: 'hi' }, { title: 'hi' }],
+      a_f: [[{ title: 'hi' }], [{ title: 'hi' }]]
     }
 
     const expect = {
+      a: 'hi',
       aB: 'a',
       aC: {
         bC: 'b',
@@ -58,7 +61,8 @@ describe('object-keys-mapping', () => {
         }
       },
       ad: [1, 2, 3],
-      ae: [{ name: 'hi' }, { name: 'hi' }, { name: 'hi' }]
+      ae: [{ name: 'hi' }, { name: 'hi' }, { name: 'hi' }],
+      aF: [[{ name: 'hi' }], [{ name: 'hi' }]]
     }
 
     it('operator.map', () => {
@@ -67,11 +71,56 @@ describe('object-keys-mapping', () => {
         mapping: {
           '.a_d': 'ad',
           '.a_e': 'ae',
-          '.a_e.title': 'name'
+          '.a_e.title': 'name',
+          '.a_f.title': 'name'
         }
       })
 
       equal(operator.map(origin), expect)
+    })
+  })
+
+  describe('camelcase: false', () => {
+    const origin = {
+      a_b: 'hi'
+    }
+
+    const expect = {
+      a_b: 'hi'
+    }
+
+    it('operator.map', () => {
+      const operator = new Operator({
+        camelcase: false
+      })
+
+      equal(operator.map(origin), expect)
+    })
+  })
+
+  describe('invalid origin', () => {
+    const operator = new Operator({
+      camelcase: true
+    })
+
+    it('null', () => {
+      equal(operator.map(null), {})
+    })
+
+    it(`''`, () => {
+      equal(operator.map(''), {})
+    })
+
+    it('undefined', () => {
+      equal(operator.map(undefined), {})
+    })
+
+    it('[]', () => {
+      equal(operator.map([]), [])
+    })
+
+    it('{}', () => {
+      equal(operator.map({}), {})
     })
   })
 })
